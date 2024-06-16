394415c34650db5066a930bda87ba5a5b7f9dc68
fischerannaa <105664355+fischerannaa@users.noreply.github.com>
2024-06-16 17:37:42 +0100

    Szöveg hozzáadása

--- weboldal.py
+++ weboldal.py
@@ -136,8 +136,13 @@ figInflationComparison.update_yaxes(range=[0, 60])
 
 # Streamlit App
 def main():
-    st.title('Projekt')
+    st.markdown("<h1 color='#68bab9' ; '>Németország, Magyarország és Bulgária makrogazdasági mutatóinak összehasonlítása és vizualizációja</h1>", unsafe_allow_html=True)
 
+    
+    st.subheader('Projekt témája')
+    st.markdown("<p style='text-align: justify;'>A következőkben három ország makrogazdasági mutatóinak (árfolyam, GDP, infláció) alakulását mutatom be a COVID-19 előtti, alatti és utáni időszakban. A vizsgált országok között szerepel egy gyengébb gazdaságú (Bulgária), egy közepesen erős gazdaságú (Magyarország), valamint egy erős gazdaságú ország (Németország). Ezáltal átfogóbb képet kaphatunk arról, hogy a COVID-19 hogyan befolyásolta egyes európai országok gazdaságát, és milyen gyors volt a helyreállítási folyamat. Az adatok forrásai a Statista, az Eurostat és a Yahoo finance. A megoldás relatíve skálázható, mivel a vizsgált országok száma bővíthető más európai országokkal vagy gazdasági mutatókkal is.</p>", unsafe_allow_html=True)
+
+    st.subheader('Árfolyamok')
     st.subheader('USD/BGN árfolyam alakulása')
     st.plotly_chart(figCurrencyHistoryBgn)
     
@@ -147,26 +152,23 @@ def main():
     st.subheader('USD/EUR árfolyam alakulása')
     st.plotly_chart(figCurrencyHistoryEur)
 
-    st.subheader('Árfolyamok alakulása')
+    st.subheader('Árfolyamok összehasonlítása')
+    
+    st.markdown("<p style='text-align: justify;'>2019 végén és 2020 elején az árfolyamok nagyjából stabilitást mutatnak, azonban 2020-ban a COVID-19 világjárvány megjelenése jelentős hatást gyakorolt a gazdaságokra és a devizapiacokra. A járvány miatti gazdasági bizonytalanság és lezárások világszerte csökkentették a gazdasági aktivitást, ami hatással volt a devizaárfolyamokra. Magyarország és Bulgária esetében látható némi ingadozás, ez a befektetők reakciója miatt van a járvány okozta változásokra. 2021-ben a USD/HUF és USD/BGN árfolyamok csökkenése figyelhető meg. 2022-ben az inflációs nyomás növekedése mind az Egyesült Államokban, mind Európában arra késztette a jegybankokat, hogy szigorítsák monetáris politikájukat. Az amerikai Federal Reserve kamatemelései különösen erősítették a dollárt.</p>", unsafe_allow_html=True)
     st.plotly_chart(figUnited)
-
+    st.subheader('GDP')
     st.plotly_chart(fig_bul)
     st.plotly_chart(fig_hun)
     st.plotly_chart(fig_ger)
-
+    st.markdown("<p style='text-align: justify;'>Mindárom ország esetén 2020-ban stagnálás figyelhető meg, ami a GDP alakulását illeti. Ez a COVID-19 járvány hatásának tudható be, viszont a GDP hamar újra növekni kezd, valamint az előrejelzések is további növekedést mutatnak.</p>", unsafe_allow_html=True)
     
+    st.subheader('Infláció')
     st.pyplot(fig)
 
     
     st.plotly_chart(figInflationComparison)
+    st.markdown("<p style='text-align: justify;'>A vizsgált országokban 2021-ben és 2022-ben jelentős inflációs hullám figyelhető meg. Bár Németországban az infláció növekedése mérsékeltebb, mint Bulgáriában és Magyarországon, itt is jelentős emelkedést tapasztaltak. Az infláció emelkedése részben az energiaárak növekedésének, az élelmiszerárak drágulásának, valamint a globális ellátási lánc problémáinak tudható be. Az Orosz-ukrán háború tovább súlyosbította a helyzetet, mivel ezek az országok nagymértékben függnek az orosz energia importtól.</p>", unsafe_allow_html=True)
     
-    # Discussion
-    st.subheader('Discussion')
-    st.write("The sales data shows a steady increase over the years. In 2010, sales were at $100, and by 2014, they had increased to $300. This indicates a positive growth trend for the company.")
-    st.write("Factors contributing to this growth could include market demand, effective marketing strategies, and improvements in product quality.")
-    st.write("However, it's important to note that without further analysis, we cannot attribute the entire increase to these factors. External factors such as economic conditions and competition may also have played a role.")
-    st.write("Further analysis could involve comparing sales growth to industry benchmarks, conducting customer surveys, or examining sales data by region or product category.")
-
 # Function to plot dat
 
 if __name__ == "__main__":
